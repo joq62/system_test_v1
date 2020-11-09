3b5727530453eddb3a9b26987a9da675b0dae2f4
joq62 <joakim.leche@gmail.com>
2020-11-09 22:56:18 +0100

    update

--- test_src/log_test.erl
+++ test_src/log_test.erl
@@ -54,13 +54,15 @@ start()->
 %% Returns: non
 %% --------------------------------------------------------------------
 test2()->
-     ?assertEqual([{error,node2,m2,l1,error,{2020,10,1},{12,0,0},"Error 1"},
-		   {error,node2,m3,l3,error,{2021,5,1},{12,0,0},"Error 2"}],
-		  db_log:severity(error)),
-     ?assertEqual([{info,'10250@asus',module1,line1,info,{1962,3,7},{22,18,34},"System start and intial init of mnesia"},
-		  {info,'10250@asus',log_test,55,info,{2020,10,1},{13,0,0},"Event 2"},
-		  {info,'10250@asus',log_test,66,info,{2020,10,1},{13,0,0},"Event 3"}],
+     ?assertEqual([{error,node2,m3,l3,{2021,5,1},{12,0,0},"Error 2"},
+		   {error,node2,m2,l1,{2020,10,1},{12,0,0},"Error 1"}],
+		  if_db:log_severity(error)),
+     ?assertEqual([{info,'10250@asus',log_test,55,{2020,10,1},{13,0,0},"Event 2"},
+		   {info,'10250@asus',log_test,66,{2020,10,1},{13,0,0},"Event 3"},
+		   {info,'10250@asus',module1,line1,{1962,3,7},{22,18,34},"System start and intial init of mnesia"}],
 		  db_log:severity(info)),
+
+    
     
     ok.
 %% Function:start/0 
@@ -71,28 +73,30 @@ test2()->
 
 
 test1()->
-    D1={2020,10,01},
-    T1={13,00,00},
-    ?assertEqual({atomic,ok},db_log:create(node(),?MODULE,55,info,D1,T1,"Event 2")),
-    
-    ?assertEqual([{info,'10250@asus',module1,line1,{1962,3,7},{22,18,34},"System start and intial init of mnesia"},
-		  {info,'10250@asus',log_test,55,{2020,10,1},{13,0,0},"Event 2"}],db_log:read_all()),
-    D2={2020,10,01},
-    T2={13,00,00},
-    ?assertEqual({atomic,ok},db_log:create(node(),?MODULE,66,info,D2,T2,"Event 3")),
-    ?assertEqual([{info,'10250@asus',module1,line1,{1962,3,7},{22,18,34},"System start and intial init of mnesia"},
-		  {info,'10250@asus',log_test,55,{2020,10,1},{13,0,0},"Event 2"},
-		  {info,'10250@asus',log_test,66,{2020,10,1},{13,0,0},"Event 3"}],db_log:read_all()),
+    ?assertEqual({atomic,ok},if_db:log_create(node(),?MODULE,55,info,{2020,10,01},{13,00,00},"Event 2")),
+    ?assertEqual([{info,'10250@asus',log_test,55,{2020,10,1},{13,0,0},"Event 2"},
+		  {info,'10250@asus',module1,line1,{1962,3,7},{22,18,34},"System start and intial init of mnesia"}],db_log:read_all()),
+ 
+    ?assertEqual({atomic,ok},db_log:create(node(),?MODULE,66,info,{2020,10,01},{13,00,00},"Event 3")),
+    timer:sleep(300),
+    ?assertEqual([{info,'10250@asus',log_test,55,{2020,10,1},{13,0,0},"Event 2"},
+		  {info,'10250@asus',log_test,66,{2020,10,1},{13,0,0},"Event 3"},
+		  {info,'10250@asus',module1,line1,{1962,3,7},{22,18,34},"System start and intial init of mnesia"}],db_log:read_all()),
 
     ?assertEqual({atomic,ok},db_log:create(node2,m2,l1,error,{2020,10,01},{12,00,00},"Error 1")),
     ?assertEqual({atomic,ok},db_log:create(node2,m3,l3,error,{2021,05,01},{12,00,00},"Error 2")),
 
-    ?assertEqual([{error,node2,m2,l1,{2020,10,1},{12,0,0},"Error 1"},
-		  {error,node2,m3,l3,{2021,5,1},{12,0,0},"Error 2"},
-		  {info,'10250@asus',module1,line1,{1962,3,7},{22,18,34},"System start and intial init of mnesia"},
+    ?assertEqual([{error,node2,m3,l3,{2021,5,1},{12,0,0},"Error 2"},
 		  {info,'10250@asus',log_test,55,{2020,10,1},{13,0,0},"Event 2"},
-		  {info,'10250@asus',log_test,66,{2020,10,1},{13,0,0},"Event 3"}],db_log:read_all()),
-    			 
+		  {info,'10250@asus',log_test,66,{2020,10,1},{13,0,0},"Event 3"},
+		  {error,node2,m2,l1,{2020,10,1},{12,0,0},"Error 1"},
+		  {info,'10250@asus',module1,line1,{1962,3,7},{22,18,34},"System start and intial init of mnesia"}],db_log:read_all()),
+
+     ?assertEqual([{error,node2,m3,l3,{2021,5,1},{12,0,0},"Error 2"},
+		  {info,'10250@asus',log_test,55,{2020,10,1},{13,0,0},"Event 2"},
+		  {info,'10250@asus',log_test,66,{2020,10,1},{13,0,0},"Event 3"}],
+		  db_log:latest(3,all)),
+
     ok.
 
 
